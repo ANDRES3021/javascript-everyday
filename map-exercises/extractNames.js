e9ed55692d927b2c90d2ae5f6b4150b2b4f25758
Andres <carlos.pardo@uptc.edu.co>
2025-07-17 20:39:09 -0500

    extraer solo los nombres de un array con objetos

---
+++ map-exercises/extractNames.js
@@ -0,0 +1,7 @@
+const people = [
+  { name: "Alice", age: 22 },
+  { name: "Bob", age: 30 },
+  { name: "Charlie", age: 25 },
+];
+const names = people.map((value) => value.name);
+console.log(names);
